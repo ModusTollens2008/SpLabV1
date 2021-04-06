e2721a7fd3bcf5150b20e4a01da7ff04966b1b57
mixail19997 <mixail19997@gmail.com>
2021-04-06 22:57:42 +0400

    hueta

---
+++ TrunoGT/BusinessLogic/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+
+    public class WorkWithFiles
+    {
+        private List<DllNode> Antonya = new List<DllNode>();
+        public getFromFile(string filename)
+        {
+           
+        }
+
+    }
+}
---
+++ TrunoGT/BusinessLogic/DllNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class DllNode
+    {
+        private string name;
+        private string version;
+        private string lastchange;
+
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Vers {
+            get {return version; }
+            set { version = value; }
+        }
+        public string Lastchange {
+            get { return lastchange; }
+            set { lastchange = value; }
+        }
+
+
+        //public _Lastchange{get {return lastchange;} }
+        public DllNode(string name, string version, string lastchange)
+        {
+            this.name = name;
+            this.version = version;
+            this.lastchange = lastchange;
+        }
+
+    }
+}
