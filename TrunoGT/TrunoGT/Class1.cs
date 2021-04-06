c411a10124e5b046bed4a1c2a7f369a05ea8ca61
pavlch pavl <pavlikov.ivan2015@yandex.ru>
2021-04-06 21:29:27 +0400

    popravil

--- TrunoGT/TrunoGT/Class1.cs
+++ TrunoGT/TrunoGT/Class1.cs
@@ -8,6 +8,6 @@ namespace TrunoGT
 {
     class Class1
     { private string hui="huiname";
-        private string zaebska = "zaebashkaname";
+        private string zaebska = "NeSilnoZaebska";
     }
 }
