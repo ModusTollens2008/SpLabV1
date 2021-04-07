4e22782093efb151e84ce7702f715a671c06db76
mixail19997 <mixail19997@gmail.com>
2021-04-07 17:16:04 +0400

    dfas

---
+++ TrunoGT/BusinessLogic/StructureAnalyse.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class StructureAnalyse
+    {
+        class CodeProvide
+        {
+            public static int CheckStucture(string code)
+            {
+                int start = IsFitVar10(code);
+                if (start == -1)
+                    return 0;
+                while (code[start] == ' ')
+                    start++;
+                switch (code[start])
+                {
+                    case ';':
+                        code = code.Insert(start, "count++");
+                        break;
+                    case '{':
+                        code = code.Insert(start + 1, "count++;");
+                        break;
+                    default:
+                        code = code.Insert(start, "{count++;");
+                        start += 9;
+                        start = code.IndexOf(';', start);
+                        if (start == -1)
+                            return 0;
+                        code = code.Insert(++start, "}");
+                        break;
+                }
+                var provider = new CSharpCodeProvider();
+                var parameters = new CompilerParameters { GenerateInMemory = true };
+                parameters.ReferencedAssemblies.Add("System.dll");
+                try
+                {
+                    var results = provider.CompileAssemblyFromSource(parameters, $@"
+                    using System;
+ 
+                    public static class Checker 
+                    {{
+                        public static int F()
+                        {{
+                            int count=0;
+                            {code}
+                            return count;
+                        }}
+                    }}");
+                    var method = results.CompiledAssembly.GetType("Checker").GetMethod("F");
+                    var func = (Func<int>)Delegate.CreateDelegate(typeof(Func<int>), null, method);
+                    return func();
+                }
+                catch (FileNotFoundException)
+                {
+                    throw new ArgumentException("Input should be valid C# expression", nameof(code));
+                }
+            }
+
+            private static int IsFitVar10(string code)
+            {
+                int startStruct = code.IndexOf("foreach");
+                if (startStruct != -1)
+                {
+                    startStruct += 7;
+                    while (startStruct < code.Length && code[startStruct] == ' ')
+                        startStruct++;
+                    if ((startStruct < code.Length) && (code[startStruct] != ')'))
+                    {
+                        startStruct = code.IndexOf(')', startStruct);
+                        if (startStruct != -1)
+                        {
+                            startStruct++;
+                        }
+                    }
+                    else
+                        startStruct = -1;
+                }
+                return startStruct;
+            }
+        }
+    }
+}
