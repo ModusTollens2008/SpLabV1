124ed78075b610b81ec273f00be1e291d4c6a123
mixail19997 <mixail19997@gmail.com>
2021-04-07 10:26:08 +0400

    hui

--- TrunoGT/BusinessLogic/WorkWithXML.cs
+++ TrunoGT/BusinessLogic/WorkWithXML.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
+using System.Xml;
 namespace BusinessLogic
 {
     class WorkWithXML
     {
+       
+        public IEnumerable<DllNode> readFile(string fileName)
+        {
+            List<DllNode> dllList = new List<DllNode>();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(fileName);
+            XmlElement xRoot = xDoc.DocumentElement;
+            string name = "";
+            string version = "";
+            string changedate = "";
+            foreach (XmlNode node in xRoot)
+            {
+                if (node.Attributes.Count > 0)
+                {
+                    XmlNode attr = node.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                    {
+                        name = attr.Value;
+                    }
+                }
+
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "version")
+                    {
+                        version = childNode.Value;
+                    }
+                    else if (childNode.Name == "changedate")
+                    {
+                        changedate = childNode.Value;
+                    }
+                }
+
+                dllList.Add(new DllNode(name, version, changedate));
+
+
+            }
+
+            return dllList;
+        }
+
+
+        public void writeFile(string fileName,List<DllNode> dllNodes)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(fileName);
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlElement xname = xDoc.CreateElement("name");
+            XmlElement xversion = xDoc.CreateElement("version");
+            XmlElement xchangedate = xDoc.CreateElement("changedate");
+
+            
+        }
     }
 }
