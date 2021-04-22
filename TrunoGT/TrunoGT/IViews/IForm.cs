298a5ac8988bf5ffb822bfbae4f520940b107082
pavlch pavl <pavlikov.ivan2015@yandex.ru>
2021-04-22 11:10:36 +0400

    hui

--- TrunoGT/TrunoGT/IViews/IForm.cs
+++ TrunoGT/TrunoGT/IViews/IForm.cs
@@ -1,13 +1,9 @@
-﻿using BusinessLogic;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TrunoGT.IViews
 {
-    interface IForm
+	interface IForm
     {
 
         string Code
--- TrunoGT/TrunoGT/Views/Form1.cs
+++ TrunoGT/TrunoGT/Views/Form1.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using BusinessLogic;
 using TrunoGT.IViews;
 using TrunoGT.Presenters;
 
