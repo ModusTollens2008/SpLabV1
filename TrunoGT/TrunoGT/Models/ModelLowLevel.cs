e823c50620adc821173a762f136d1d6822985000
mixail19997 <mixail19997@gmail.com>
2021-04-22 10:35:38 +0400

    sda

---
+++ TrunoGT/TrunoGT/Models/ModelLowLevel.cs
@@ -0,0 +1,23 @@
+﻿using LowFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrunoGT.Models
+{
+    class ModelLowLevel
+    {
+        private LowLevelFunctions _ILow=new LowLevelFunctions();
+        public int XOR(int a)
+        {
+           return _ILow.LowLevelNot(a);
+        }
+        public int Mul(int a, int b)
+        {
+            return _ILow.LowLevelMultiple(a,b);
+        }
+
+    }
+}
